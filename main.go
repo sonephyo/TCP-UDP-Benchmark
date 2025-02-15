604d256d13f11b215d1c5637095883e2765c2647
Soney <sonephyo7777777@gmail.com>
2025-02-15 18:30:05 -0500

    doc: updating and adding some comments

--- main.go
+++ main.go
@@ -5,16 +5,6 @@ import (
 )
 
 func main() {
-
-	text := "Hello World"
-
-    encodedBytes :=  xorEncodeDecode([]byte(text), uint64(1343123213123434))
-    fmt.Println(encodedBytes)
-    decodedBytes := xorEncodeDecode(encodedBytes, uint64(1343123213123434))
-    fmt.Println(string(decodedBytes[:]))
-    // fmt.Println(secret_key)
-    // decodedTest := xorEncodeDecode([]byte(text), uint64(secret_key))
-    // fmt.Println(decodedTest)
-    // fmt.Println(secret_key)
+    // The code from this file are for testing. Please refer to the specific folder for in-depth testing.
 
 }
