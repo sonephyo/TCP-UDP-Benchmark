e7b9fc3d429ec9fb13b2146b24eefe2b9778f1a9
Soney <sonephyo7777777@gmail.com>
2025-03-02 01:49:32 -0500

    issue: data hashing inconsistany

--- clientSocket/main.go
+++ clientSocket/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"bufio"
+	// "bufio"
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
@@ -21,8 +22,24 @@ func xorEncodeDecode(text []byte, key *uint64) []byte {
 		encryptedLi[i] = text[i] ^ byte(*key)
 	}
 	*key = xorShift(*key)
-	fmt.Println("key isssss: ", *key)
 	return encryptedLi
+}
+
+func sendDataToClient(msg string, key *uint64, conn net.Conn, bufferSize int) {
+	msgEncoded := xorEncodeDecode([]byte(msg), key)
+
+	lengthBuffer := make([]byte, 4)
+	binary.BigEndian.PutUint32(lengthBuffer, uint32(len(msgEncoded)))
+	conn.Write(lengthBuffer)
+	fmt.Printf("Sending with byte size of %d\n", bufferSize)
+	for i := 0; i < len(msgEncoded); i += bufferSize {
+		end := i + bufferSize
+		if end > len(msgEncoded) {
+			end = len(msgEncoded)
+		}
+		chunk := msgEncoded[i:end]
+		conn.Write(chunk)
+	}
 }
 
 func main() {
@@ -40,7 +57,7 @@ func main() {
 	}
 
 	// Creating connection with the server
-	scanner := bufio.NewScanner(os.Stdin)
+	// scanner := bufio.NewScanner(os.Stdin)
 	conn, err := net.Dial("tcp", hostAddress)
 	if err != nil {
 		fmt.Println("Error: ", err)
@@ -49,28 +66,38 @@ func main() {
 	key := uint64(1343123213123434)
 
 	defer conn.Close()
-	for {
-		fmt.Print("Enter message: ")
-		if !scanner.Scan() {
-			break
+	bufferSizes := []int{8, 64, 256, 512}
+
+	for _, longMessage := range LongMessages {
+		for _, value := range bufferSizes {
+			start := time.Now()
+			sendDataToClient(longMessage, &key, conn, value)
+			elapsed := time.Since(start)
+			fmt.Println("Elapsed time:", elapsed)
+			fmt.Println("----")
 		}
-		msg := scanner.Text()
-
-		if msg == "exit" {
-			break
-		}
-
-		start := time.Now()
-
-		msgEncoded := xorEncodeDecode([]byte(msg), &key)
-		fmt.Println(msgEncoded)
-		_, err = conn.Write(msgEncoded)
-		if err != nil {
-			fmt.Println("Error: ", err)
-			return
-		}
-
-		elapsed := time.Since(start)
-		fmt.Println("Elapsed time:", elapsed)
 	}
+
+	// for {
+	// 	fmt.Print("Enter message: ")
+	// 	if !scanner.Scan() {
+	// 		break
+	// 	}
+	// 	msg := scanner.Text()
+
+	// 	if msg == "exit" {
+	// 		break
+	// 	}
+
+	// 	bufferSizes := []int{8, 64, 256, 512}
+
+	// 	for _, value := range bufferSizes {
+	// 		start := time.Now()
+	// 		sendDataToClient(msg, &key, conn, value)
+	// 		elapsed := time.Since(start)
+	// 		fmt.Println("Elapsed time:", elapsed)
+	// 		fmt.Println("----")
+	// 	}
+
+	// }
 }
